7302fd56c5d537656225a29d55537bcb5576d67b
aymn <aymn.ali.ruh.k@tuwaiq.edu.sa>
2021-12-21 19:59:47 +0300

    googleAuth

---
+++ app/src/main/java/com/example/knowmeapp/ListOfContactFragment.kt
@@ -0,0 +1,67 @@
+package com.example.knowmeapp
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.knowmeapp.databinding.FragmentListOfContactBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class ListOfContactFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    private var _binding: FragmentListOfContactBinding?= null
+    private val binding get() = _binding
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentListOfContactBinding.inflate(inflater,container,false)
+        setHasOptionsMenu(true)
+        return binding?.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.user_profile, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) =when(item.itemId){
+        R.id.singOut -> {
+            singOut()
+        true
+        }
+        else -> false
+    }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        auth = Firebase.auth
+        binding?.textView?.text = "${auth.currentUser?.displayName}"
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.web_clint_id))
+            .requestEmail().build()
+
+        googleSignInClient = GoogleSignIn.getClient(context,gso)
+
+
+    }
+    private fun singOut(){
+        Firebase.auth.signOut()
+        googleSignInClient.signOut()
+        val action = ListOfContactFragmentDirections.actionListOfContactFragmentToSingInFragment()
+        findNavController().navigate(action)
+    }
+}
