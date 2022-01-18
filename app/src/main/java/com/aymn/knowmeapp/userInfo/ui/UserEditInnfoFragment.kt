b25e114e97ff703a6a58a0bc5caf074eddcffa0d
aymn <aymn.ali.ruh.k@tuwaiq.edu.sa>
2022-01-18 11:22:21 +0300

    - writ QR Code for the user

--- app/src/main/java/com/aymn/knowmeapp/userInfo/ui/UserEditInnfoFragment.kt
+++ app/src/main/java/com/aymn/knowmeapp/userInfo/ui/UserEditInnfoFragment.kt
@@ -1,9 +1,13 @@
 package com.aymn.knowmeapp.userInfo.ui
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.graphics.createBitmap
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -17,6 +21,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
+import com.google.zxing.qrcode.QRCodeWriter
 
 
 class UserEditInnfoFragment : Fragment() {
@@ -103,6 +110,29 @@ class UserEditInnfoFragment : Fragment() {
                 UserEditInnfoFragmentDirections.actionUserEditInnfoFragmentToListOfPersonsFragment()
             findNavController().navigate(action)
         }
+        viewModel.user.observe(viewLifecycleOwner,{UserInfo ->
+        binding?.qrGnarete?.setOnClickListener {
+            if ( UserInfo.name.isNullOrBlank()){
+                Toast.makeText(context, "Name or Number or Email is empty", Toast.LENGTH_SHORT).show()
+            } else{
+                val writer = QRCodeWriter()
+                try {
+                val bitMatrix = writer.encode(UserInfo.toString(),BarcodeFormat.QR_CODE,512,512)
+                   val hight = bitMatrix.height
+                    val width = bitMatrix.width
+                    val bmp = Bitmap.createBitmap(width,hight,Bitmap.Config.RGB_565)
+                    for (x in 0 until width){
+                        for (y in 0 until hight){
+                            bmp.setPixel(x,y,if (bitMatrix[x,y])Color.BLACK else Color.WHITE)
+                        }
+                    }
+                    binding?.qrCodeImage?.setImageBitmap(bmp)
+                }catch (e:WriterException){
+                    e.printStackTrace()
+                }
+            }
+
+        }})
     }
 
     private fun singOut() {
